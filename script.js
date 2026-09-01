6b3c7e8a3486baff5d49ca9c0410f5bca7932239
mariaclarataborda4-design <mariaclarataborda4@gmail.com>
2026-09-01 09:02:36 -0300

    ok

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListene("DOMContentLoaded", () => {
+    const likeBtn = document.querySelector(".like-btn");
+    const postMedia = document.querySelector(".post-media")
+if (!likeBtn) return;
+
+const likesCountSpan = likeBtn.querySelector(".like-count");
+const bookmarkBtn = document.querySelector("bookmark-btn");
+
+
+
+
+
+})
